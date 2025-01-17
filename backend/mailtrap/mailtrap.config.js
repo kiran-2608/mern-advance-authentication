acef17b277a46f9d88e0621d59592d3d793d00b3
kiran-2608 <kirankumarbehera2608@gmail.com>
2025-01-18 00:28:09 +0530

    Project Completed

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,18 @@
+import {MailtrapClient} from 'mailtrap'
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+
+export const mailtrapClient = new MailtrapClient({
+  token: TOKEN,
+});
+
+export const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Kiran Kumar Behera",
+};
+
+
